92e5713874db9189697edb51b8618a29e14d8e42
Jared Tyler <jared@mappedin.ca>
2019-05-10 14:33:03 -0400

    Get options page working properly.

--- options.js
+++ options.js
@@ -17,21 +17,21 @@ document.addEventListener('DOMContentLoaded', function() {
 		document.getElementById('hdnDebugMode').value = isDebugMode;
 	};
 
-	document.getElementById('btnSave').onClick = function() {
+	document.getElementById('btnSave').onclick = function() {
 		var clientId = document.getElementById('txtClientId').value;
 		var clientSecret = document.getElementById('txtClientSecret').value;
 		var venueSlug = document.getElementById('txtVenueSlug').value;
-		var isDebugMode = document.getElementById('hdnDebugMode').value;
+		var isDebugMode = document.getElementById('hdnDebugMode').value === 'true';
 
 		jmtyler.log('saving settings...', { clientId, clientSecret, venueSlug, isDebugMode });
-		return;
+
 		jmtyler.settings.set('client_id', clientId)
 			.set('client_secret', clientSecret)
 			.set('venue_slug', venueSlug)
 			.set('is_debug_mode', isDebugMode);
 	};
 
-	document.getElementById('btnReset').onClick = function() {
+	document.getElementById('btnReset').onclick = function() {
 		jmtyler.settings.clear();
 		_initializeSettings();
 	};
